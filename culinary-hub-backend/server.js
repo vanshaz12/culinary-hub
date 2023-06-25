2a2f0661cca9322d943932acfbcfd20556fd3ae9
vanshaz12 <129361086+vanshaz12@users.noreply.github.com>
2023-06-25 23:11:37 +0930

    recipe details

--- culinary-hub-backend/server.js
+++ culinary-hub-backend/server.js
@@ -1,93 +1,118 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const PORT = 3001;
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 3001
 const fetch = require('node-fetch')
 
-app.use(cors());
+app.use(cors())
 
 // Import the necessary dependencies for interacting with the database
-const db = require('./db/db');
-const bcrypt = require('bcrypt');
+const db = require('./db/db')
+const bcrypt = require('bcrypt')
 
-app.listen(PORT, () => console.log(`Server is listening here: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server is listening here: http://localhost:${PORT}`))
 
-app.use(express.json());
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the server');
-});
+    res.send('Welcome to the server')
+})
 
 
 // Handle user registration
 app.post('/api/signup', async (req, res) => {
     try {
-        const { name, email, password } = req.body;
+        const { name, email, password } = req.body
 
         // Check if the user already exists
-        const userExists = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+        const userExists = await db.query('SELECT * FROM users WHERE email = $1', [email])
         if (userExists.rows.length > 0) {
-            return res.status(400).json({ message: 'User already exists' });
+            return res.status(400).json({ message: 'User already exists' })
         }
 
         // Hash the password
-        const saltRounds = 10;
-        const passwordDigest = await bcrypt.hash(password, saltRounds);
+        const saltRounds = 10
+        const passwordDigest = await bcrypt.hash(password, saltRounds)
 
         // Insert the user profile into the database
         const newUser = await db.query(
             'INSERT INTO users (name, email, password_digest) VALUES ($1, $2, $3) RETURNING *',
             [name, email, passwordDigest]
-        );
+        )
 
-        res.status(201).json({ message: 'User registered successfully', user: newUser.rows[0] });
+        res.status(201).json({ message: 'User registered successfully', user: newUser.rows[0] })
     } catch (error) {
-        console.error('Error occurred during user registration:', error);
-        res.status(500).json({ message: 'Internal server error' });
+        console.error('Error occurred during user registration:', error)
+        res.status(500).json({ message: 'Internal server error' })
     }
-});
+})
 
 // Handle user login
 app.post('/api/login', async (req, res) => {
     try {
-        const { email, password } = req.body;
+        const { email, password } = req.body
 
         // Check if the user exists
-        const user = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+        const user = await db.query('SELECT * FROM users WHERE email = $1', [email])
         if (user.rows.length === 0) {
-            return res.status(404).json({ message: 'User not found' });
+            return res.status(404).json({ message: 'User not found' })
         }
 
         // Compare the provided password with the stored password
-        const match = await bcrypt.compare(password, user.rows[0].password_digest);
+        const match = await bcrypt.compare(password, user.rows[0].password_digest)
         if (!match) {
-            return res.status(401).json({ message: 'Invalid password' });
+            return res.status(401).json({ message: 'Invalid password' })
         }
 
         // Login successful
-        res.status(200).json({ message: 'Login successful' });
+        res.status(200).json({ message: 'Login successful' })
     } catch (error) {
-        console.error('Error occurred during login:', error);
-        res.status(500).json({ message: 'Internal server error' });
+        console.error('Error occurred during login:', error)
+        res.status(500).json({ message: 'Internal server error' })
     }
-});
+})
 
-const API_KEY = 'bdcfcfd9559641359e2535faefbfa73d';
+const API_KEY = 'bdcfcfd9559641359e2535faefbfa73d'
 
 
 app.get('/api/search-recipes', async (req, res) => {
     try {
-        const { query } = req.query;
-        const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=${query}`);
+        const { query } = req.query
+        const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=${query}`)
         if (response.ok) {
-            const data = await response.json();
-            res.json(data);
+            const data = await response.json()
+            res.json(data)
         } else {
-            console.error('Error occurred during recipe search:', response.statusText);
-            res.status(500).json({ error: 'Internal server error' });
+            console.error('Error occurred during recipe search:', response.statusText)
+            res.status(500).json({ error: 'Internal server error' })
         }
     } catch (error) {
-        console.error('Error occurred during recipe search:', error);
-        res.status(500).json({ error: 'Internal server error' });
+        console.error('Error occurred during recipe search:', error)
+        res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
+app.get('/api/recipes/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        // Fetch the recipe details from the Spoonacular API using the provided ID
+        const response = await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`);
+        if (response.ok) {
+            const recipe = await response.json();
+
+            // Extract the necessary recipe details
+            const { title, image, summary, extendedIngredients, instructions } = recipe;
+
+            // Return the recipe details
+            res.status(200).json({ title, image, summary, ingredients: extendedIngredients, instructions });
+        } else {
+            console.error('Error occurred while fetching recipe details:', response.statusText);
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    } catch (error) {
+        console.error('Error occurred while fetching recipe details:', error);
+        res.status(500).json({ message: 'Internal server error' });
     }
 });
+
--- culinary-hub/src/component/Pages/RecipeDetails.js
+++ culinary-hub/src/component/Pages/RecipeDetails.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const RecipeDetails = () => {
-    const { id } = useParams();
     const [recipe, setRecipe] = useState(null);
+    const { id } = useParams();
 
     useEffect(() => {
         const fetchRecipeDetails = async () => {
             try {
-                const response = await fetch(`/api/recipe-details?id=${id}`);
+                const response = await fetch(`http://localhost:3001/api/recipes/${id}`);
                 if (response.ok) {
                     const data = await response.json();
                     setRecipe(data);
@@ -24,14 +24,29 @@ const RecipeDetails = () => {
     }, [id]);
 
     if (!recipe) {
-        return <div>Loading...</div>;
+        return <div>Loading recipe details...</div>;
     }
+
+    // Destructure the recipe object
+    const { title, image, description, ingredients, instructions } = recipe;
 
     return (
         <div>
-            <h2>{recipe.title}</h2>
-            <p>{recipe.description}</p>
-            {/* Render other details about the recipe */}
+            <h2>{title}</h2>
+            <img src={image} alt={title} />
+            <p>{description}</p>
+            <h3>Ingredients:</h3>
+            <ul>
+                {ingredients.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
+                ))}
+            </ul>
+            <h3>Instructions:</h3>
+            <ol>
+                {instructions.map((instruction, index) => (
+                    <li key={index}>{instruction}</li>
+                ))}
+            </ol>
         </div>
     );
 };
