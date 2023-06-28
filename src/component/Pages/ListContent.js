a59ae9043101fb02c479fe313d34c78f527f6eb4
vanshaz12 <129361086+vanshaz12@users.noreply.github.com>
2023-06-28 15:28:32 +0930

    lists working- with auth

--- src/component/Pages/ListContent.js
+++ src/component/Pages/ListContent.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Typography, List, ListItem, ListItemText } from '@mui/material';
 
 const ListItemDetails = () => {
     const { id } = useParams();
+    const [listItem, setListItem] = useState(null);
 
-    // Fetch the list item details using the id
+    useEffect(() => {
+        // Fetch the list item details using the id
+        const fetchListItem = async () => {
+            try {
+                const response = await fetch(`/api/lists/${id}`);
+                if (response.ok) {
+                    const listItemData = await response.json();
+                    setListItem(listItemData);
+                } else {
+                    console.error('Error occurred while fetching list item details:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error occurred while fetching list item details:', error);
+            }
+        };
+
+        fetchListItem();
+    }, [id]);
 
     return (
         <div>
-            <h1>List Item Details - {id}</h1>
-            {/* Display the details of the list item */}
+            {listItem && (
+                <div>
+                    <Typography variant="h4">{listItem.name}</Typography>
+                    <Typography variant="h6">List Item Details - {id}</Typography>
+                    <Typography variant="h6">Recipes:</Typography>
+                    {listItem.recipes && listItem.recipes.length > 0 ? (
+                        <List>
+                            {listItem.recipes.map((recipe) => (
+                                <ListItem key={recipe.id}>
+                                    <ListItemText primary={`Recipe Name: ${recipe.name}`} />
+                                    <ListItemText primary={`Preparation Time: ${recipe.preparationTime}`} />
+                                    <ListItemText primary={`Ingredients: ${recipe.ingredients}`} />
+                                    {/* Display other recipe details as needed */}
+                                </ListItem>
+                            ))}
+                        </List>
+                    ) : (
+                        <Typography variant="body1">No recipes found for this list item.</Typography>
+                    )}
+                </div>
+            )}
         </div>
     );
 };
